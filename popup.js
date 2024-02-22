262f2e71df2e49cdd65e5d65df0209436f6acf91
ishant-04 <98465616+ishant-04@users.noreply.github.com>
2024-02-22 17:30:23 +0530

    final prototype

--- popup.js
+++ popup.js
@@ -1,3 +1,22 @@
 document.getElementById('startButton').addEventListener('click', () => {
     chrome.runtime.sendMessage({action: 'start'});
 });
+
+document.getElementById('uploadButton').addEventListener('click', function() {
+    const question = document.getElementById('textInput').value;
+    console.log(question);
+    fetch('http://localhost:3000/chat', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'question': question
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('response').innerText = data.message.text;
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+});
